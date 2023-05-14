6017505bb365148996debf9d035c91431834f063
SilviaLaurella <132668099+SilviaLaurella@users.noreply.github.com>
2023-05-14 17:49:51 +0200

    Added current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#percentile");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#windValue");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "17ad6e67aa629189f73b053634668b20";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
